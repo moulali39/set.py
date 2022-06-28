91de259917cdaf0a99d91faaea068553abcd069a
moulali39 <107177595+moulali39@users.noreply.github.com>
2022-06-28 11:55:13 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,64 @@
+# a={1,2,3};b={2,3,4};c={3,4,5};d={5,6,7}
+# print(a.intersection(b))
+# print(a.intersection(c))
+# print(a.intersection(d))
+
+
+
+# """""union"""
+# print(a.union(b))
+# print(a.union(d))
+# print(b.union(c))
+
+
+
+# ""'""'update """""""
+# a.update(c)
+# print(a)
+# a.update(d)
+# # print(a)
+
+
+# """" difference """
+# print(a.difference(b))
+# print(b.difference(a))
+# print(a-b)
+# print(b-a)
+
+# """""" discard"""""
+
+# a={1,2,3,4}
+# b={3,4,5,6}
+# a.discard(4)
+# # print(a)
+# print(b)
+# b.discard(4)
+# print(b)
+
+# """pop"""
+
+
+# a={1,2,3,4}
+# a.pop()
+# print(a)
+# a.pop()
+# print(a)
+
+
+# """" disjoin """
+
+
+# a={1,2,3,4}
+# b={5,6,7,8}
+# print('are  a and b disjoint',a.isdisjoint(b))
+# print('are  a and b disjoint',a.isdisjoint(b))
+# print('are  a and b disjoint',a.isdisjoint(b))
+
+# """""issubset"""
+
+# a={1,2,3,4}
+# b={3,4,7,8}
+# c={4,5,6,7}
+# print(a.issubset(b))
+# print(a.issubset(c))
+
